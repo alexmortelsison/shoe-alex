563b8ca946415cb26cc4a88ae043287bbd62bdc2
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-10-27 20:12:11 +0900

    updated

---
+++ app/actions.ts
@@ -0,0 +1,22 @@
+"use server";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+import { parseWithZod } from "@conform-to/zod";
+import { productSchema } from "@/lib/zodSchema";
+
+export async function createProduct(prevState: unknown, formData: FormData) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user || user.email !== "alexmortelsison@gmail.com") {
+    return redirect("/");
+  }
+
+  const submission = parseWithZod(formData, {
+    schema: productSchema,
+  });
+
+  if (submission.status !== "success") {
+    return submission.reply();
+  }
+}
--- app/dashboard/products/create/page.tsx
+++ app/dashboard/products/create/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { createProduct } from "@/app/actions";
 import { UploadDropzone } from "@/app/lib/uploadthing";
 import { Button } from "@/components/ui/button";
 import {
@@ -20,14 +21,26 @@ import {
 import { Switch } from "@/components/ui/switch";
 import { Textarea } from "@/components/ui/textarea";
 import { SelectValue } from "@radix-ui/react-select";
-import { error } from "console";
 import { ChevronLeft } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import { useFormState } from "react-dom";
+import { useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
+import { productSchema } from "@/lib/zodSchema";
 
 export default function ProductCreateRoute() {
+  const [lastResult, action] = useFormState(createProduct, undefined);
+  const [form, fields] = useForm({
+    lastResult,
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema: productSchema });
+    },
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
+  });
   return (
-    <form>
+    <form id={form.id} onSubmit={form.onSubmit} action={action}>
       <div className="flex items-center gap-4">
         <Button variant="outline" size="icon" asChild>
           <Link href="/dashboard/products">
@@ -49,9 +62,14 @@ export default function ProductCreateRoute() {
               <Label>Name</Label>
               <Input
                 type="text"
+                key={fields.name.key}
+                name={fields.name.name}
+                defaultValue={fields.name.initialValue}
                 className="w-full"
                 placeholder="Product Name"
-              ></Input>
+              />
+
+              <p className="text-red-500">{fields.name.errors}</p>
             </div>
             <div className="flex flex-col gap-3">
               <Label>Description</Label>
